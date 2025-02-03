395dc4d7dc0379000fa414c0c8b69fc61786d828
Eduard <barned85@gmail.com>
2025-02-03 21:30:34 +0300

    add requirements add catalog

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Category, Product, Contacts
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name_category',)
+    search_field = ('name_category', 'description',)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name_product', 'price', 'category',)
+    list_filter = ('category',)
+    search_field = ('name_product', 'description',)
+
+
+@admin.register(Contacts)
+class ContactsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'country', 'address')
--- catalog/views.py
+++ catalog/views.py
@@ -1,15 +1,25 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from .models import Product, Contacts
+
 
 def home(request):
+    latest_products = Product.objects.order_by('created_at')[:5]
+
+    for product in latest_products:
+        print(
+            f'{product.name_product}: {product.description}. Дата создания: {product.created_at}. Цена: {product.price}')
+
     return render(request, 'home.html')
 
 
 def contacts(request):
+    contacts_list = Contacts.objects.all()
+
     if request.method == 'POST':
         name = request.POST.get('name')
         phone = request.POST.get('phone')
         message = request.POST.get('message')
         return HttpResponse(f"Спасибо, {name}! Мы обязательно с вами свяжемся.")
-    return render(request, 'contacts.html')
+    return render(request, 'contacts.html', {'contacts': contacts_list})
